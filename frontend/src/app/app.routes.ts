7fddb712cb63c92a66abe8228f9556997a9bc9fa
Oleksandr Boretskyi <olexandr.boretskyi@gmail.com>
2024-10-25 22:18:09 +0200

    update components

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { SubscriptionsComponent } from './components/subscriptions/subscriptions.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
+import { NotificationSettingsComponent } from './components/notification-settings/notification-settings.component';
+import { FormComponent } from './components/form/form.component';
+import { ListComponent } from './components/list/list.component';
 
-export const routes: Routes = [];
+// we import routes to components here
+
+const routes: Routes = [
+    { path: '', component: SubscriptionsComponent },
+    { path: 'add-subscription', component: FormComponent },
+    { path: 'settings', component: NotificationSettingsComponent },
+    { path: 'calendar', component: CalendarComponent }, 
+    { path: 'list', component: ListComponent }
+  ];
+
+  export default routes;
---
+++ frontend/src/app/components/subscriptions/subscriptions.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subscriptions',
+  standalone: true,
+  imports: [],
+  templateUrl: './subscriptions.component.html',
+  styleUrl: './subscriptions.component.css',
+  template: `<h1>Subscriptions</h1>`,
+})
+export class SubscriptionsComponent {
+
+}
